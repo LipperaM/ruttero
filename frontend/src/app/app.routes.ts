ba03d67d1f95aa0e57e57b4551a4215fd699aac2
Martin Lippera <martinlippera@gmail.com>
2026-01-06 14:43:57 -0300

    Add reset-password & trips routes to router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component'; 
+import { TripsComponent } from './components/trips/trips.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
@@ -10,5 +12,7 @@ export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'reset-password', component: ResetPasswordComponent },
+  { path: 'trips', component: TripsComponent, canActivate: [authGuard] },
   { path: '**', redirectTo: '' }
 ];
