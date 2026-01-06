e56bbbbc2bc2c7341dcad8138f2ccaca2fda7dea
Martin Lippera <martinlippera@gmail.com>
2026-01-06 14:53:23 -0300

    Create trips component with initial mockup data

---
+++ frontend/src/app/components/trips/trips.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-trips',
+  standalone: true,
+  imports: [],
+  templateUrl: './trips.component.html',
+  styleUrls: ['./trips.component.css'],
+})
+export class TripsComponent {
+
+}
